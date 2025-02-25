cdc9b2222e242c1398239a8472b4c93a92cd81da
Zofia Nora <sophienora92@gmail.com>
2025-02-25 10:03:21 +0100

    added css file

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
+import './App.css';
 
 export default function App() {
   return (
